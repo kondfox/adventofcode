6bf0662639afd722954aad9227a0507ef8ebcfc7
Péter Tamás <kondvik@gmail.com>
2024-12-01 13:11:11 +0100

    feat(2024/dec_01): Refactor parsing

--- 2024/dec_01/dec01.js
+++ 2024/dec_01/dec01.js
@@ -1,22 +1,19 @@
 export function part1(input) {
-  const [list1, list2] = input
-    .map(row => row.split('   '))
-    .reduce((lists, row) => {
-      lists[0].push(row[0])
-      lists[1].push(row[1])
-      return lists
-    }, [[], []])
-    .map(list => list.sort((a, b) => a - b))
+  const [list1, list2] = parse(input).map(list => list.sort((a, b) => a - b))
   return list1.reduce((diffs, num1, i) => diffs + Math.abs(num1 - list2[i]), 0)
 }
 
 export function part2(input) {
-  const [list1, list2] = input
+  const [list1, list2] = parse(input)
+  return list1.reduce((similarity, n1) => similarity + n1 * list2.filter(n2 => n1 === n2).length, 0)
+}
+
+function parse(input) {
+  return input
     .map(row => row.split('   '))
     .reduce((lists, row) => {
       lists[0].push(row[0])
       lists[1].push(row[1])
       return lists
     }, [[], []])
-  return list1.reduce((similarity, num1) => similarity + num1 * list2.filter(num2 => num1 === num2).length, 0)
 }
