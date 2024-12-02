64dd147b8a78d9235d662bef0006e3bf8cf7b2c6
Péter Tamás <kondvik@gmail.com>
2024-12-02 21:29:55 +0100

    refactor(2024/dec_02): Refactor solution

--- 2024/js/dec_02/dec02.js
+++ 2024/js/dec_02/dec02.js
@@ -1,45 +1,32 @@
 export function part1(input) {
   return parse(input)
-    .filter(report => isSafe(report))
+    .filter(isValid)
     .length
 }
 
 export function part2(input) {
   return parse(input)
-    .filter(report => isSafe2(report))
+    .map(report => ({ report, firstBad: firstBadIndex(report) }))
+    .filter(r => (r.firstBad === -1 || isTolerable(r.report, r.firstBad)))
     .length
 }
 
-function isSafe(report) {
-  if (report.length < 2) return true
-  const isAscending = report[1] > report[0]
-  for (let i = 1; i < report.length; i++) {
-    if (
-      report[i] === report[i - 1] ||
-      Math.abs(report[i] - report[i - 1]) > 3 ||
-      (report[i] < report[i - 1] && isAscending) ||
-      (report[i] > report[i - 1] && !isAscending)
-    ) return false
-  }
-  return true
+function isValid(report) {
+  return firstBadIndex(report) === -1
 }
 
-function isSafe2(report, hadBadLevel = false) {
-  if (report.length < 2) return true
-  for (let i = 1; i < report.length - 1; i++) {
-    const [prevI, actI, nextI] = [i - 1, i, i + 1]
-    if (!isSafe([report[prevI], report[actI], report[nextI]])) {
-      if (hadBadLevel) return false
-      const prevDropped = isSafe2(report.slice(0, prevI).concat(report.slice(actI)), true)
-      const actDropped = isSafe2(report.slice(0, actI).concat(report.slice(nextI)), true)
-      const nextDropped = isSafe2(report.slice(0, nextI).concat(report.slice(nextI + 1)), true)
-      return prevDropped || actDropped || nextDropped
-    }
-  }
-  return true
+function firstBadIndex(report) {
+  return report.findIndex((n, i) => !isSafePair(report[i - 1], n, report[0] < report[1]))
+}
+
+function isTolerable(report, i) {
+  return [0, 1, 2].some(j => isValid(report.slice(0, i - j).concat(report.slice(i - j + 1))))
+}
+
+function isSafePair(n, m, isAsc) {
+  return !n || !m || (n !== m && Math.abs(n - m) <= 3 && (isAsc ? n < m : n > m))
 }
 
 function parse(input) {
-  return input
-    .map(line => line.split(' ').map(n => parseInt(n)))
+  return input.map(line => line.split(' ').map(n => parseInt(n)))
 }
