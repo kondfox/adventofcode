cc5b597186b79b4fc5c61aa73c16475b4188cc1f
Péter Tamás <kondvik@gmail.com>
2024-12-04 22:42:13 +0100

    refactor(2024/js): Refactor dec_04 solution

--- 2024/js/dec_04/dec04.js
+++ 2024/js/dec_04/dec04.js
@@ -1,77 +1,41 @@
 export function part1(input) {
-  const h = input.reduce((acc, row) => acc + horizontal(row), 0)
-  const v = rotate(input).reduce((acc, row) => acc + horizontal(row), 0)
-  const d = diagonal(input)
-  return h + v + d
-}
-
-// M.S  S.S  M.M  S.M
-// .A.  .A.  .A.  .A.
-// M.S  M.M  S.S  S.M
-
-export function part2(input) {
-  let count = 0
-  for (let rowI = 1; rowI < input.length - 1; rowI++) {
-    for (let colI = 1; colI < input[rowI].length - 1; colI++) {
-      if (input[rowI][colI] === 'A') {
-        const main = mainDiagonal3(input, rowI, colI)
-        const secondary = secondaryDiagonal3(input, rowI, colI)
-        if ((main === 'SAM' || main === 'MAS') && (secondary === 'SAM' || secondary === 'MAS')) {
-          console.log(`[${rowI}, ${colI}]`)
-          count++
-        }
-      }
-    }
-  }
-  return count
-}
-
-function mainDiagonal3(input, rowI, colI) {
-  return input[rowI - 1][colI - 1] + input[rowI][colI] + input[rowI + 1][colI + 1]
-}
-
-function secondaryDiagonal3(input, rowI, colI) {
-  return input[rowI - 1][colI + 1] + input[rowI][colI] + input[rowI + 1][colI - 1]
+  return horizontal(input) + horizontal(rotate(input)) + diagonal(input)
 }
 
 function horizontal(input) {
-  let count = 0
-  for (let i = 0; i < input.length - 3; i++) {
-    const word = input.slice(i, i + 4)
-    if (word === 'XMAS' || word === 'SAMX') count++
-  }
-  return count
+  return input.reduce((acc, row) => acc + [...row.matchAll(/(?=(XMAS|SAMX))/g)].length, 0)
 }
 
 function diagonal(input) {
-  let count = 0
-  for (let rowI = 0; rowI < input.length; rowI++) {
-    for (let colI = 0; colI < input[rowI].length; colI++) {
-      if (rowI < input.length - 3 && colI < input[rowI].length - 3) {
-        let mainDiagonal = ''
-        for (let i = 0; i < 4; i++) {
-          mainDiagonal += input[rowI + i][colI + i]
-        }
-        if (mainDiagonal === 'XMAS' || mainDiagonal === 'SAMX') count++
-      }
-      if (rowI < input.length - 3 && colI >= 3) {
-        let secondaryDiagonal = ''
-        for (let i = 0; i < 4; i++) {
-          secondaryDiagonal += input[rowI + i][colI - i]
-        }
-        if (secondaryDiagonal === 'XMAS' || secondaryDiagonal === 'SAMX') count++
-      }
-    }
-  }
-  return count
+  return input.reduce((count, row, rowI) => {
+    return count + [...row].reduce((rowCount, _, colI) => {
+      return rowCount + checkDiagonal(input, rowI, colI, 1) + checkDiagonal(input, rowI, colI, -1)
+    }, 0)
+  }, 0)
 }
 
+function checkDiagonal(input, rowI, colI, dir) {
+  return ['XMAS', 'SAMX']
+    .includes(Array.from({ length: 4 },(_, i) => input[rowI + i]?.[colI + i * dir]).join('')) ? 1 : 0
+};
+
 function rotate(input) {
-  const rotated = new Array(input[0].length).fill(null).map(() => new Array(input.length).fill(null))
-  for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input[i].length; j++) {
-      rotated[j][i] = input[i][j]
-    }
-  }
-  return rotated.map(row => row.join(''))
+  return Array.from({ length: input[0].length },
+    (_, i) => Array.from({ length: input.length },
+      (_, j) => input[j][i]).join(''))
+}
+
+export function part2(input) {
+  return input.reduce((count, row, rowI) => {
+    return count + [...row].reduce((rowCount, s, colI) => {
+      return s === 'A' && checkDiagonal2(input, rowI, colI, 1) && checkDiagonal2(input, rowI, colI, -1)
+        ? rowCount + 1
+        : rowCount
+    }, 0)
+  }, 0)
+}
+
+function checkDiagonal2(input, rowI, colI, dir) {
+  return ['SAM', 'MAS']
+    .includes(input[rowI - 1]?.[colI - dir] + input[rowI]?.[colI] + input[rowI + 1]?.[colI + dir])
 }
