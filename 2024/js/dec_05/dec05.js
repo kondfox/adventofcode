442082792d4bd310c9b48a3f0ccf64c043cc054d
Péter Tamás <kondvik@gmail.com>
2024-12-05 22:46:58 +0100

    refactor(2024/js): Refactor dec_05 solution

--- 2024/js/dec_05/dec05.js
+++ 2024/js/dec_05/dec05.js
@@ -1,19 +1,18 @@
 export function part1(input) {
-  const { rules, pages } = parse(input)
-  return pages
-    .filter(page => areCorrectPages(page, constructRuleMap(rules)))
-    .map(correct => correct[Math.floor(correct.length / 2)])
-    .reduce((acc, n) => acc + n, 0)
+  return solve(input, true)
 }
 
 export function part2(input) {
+  return solve(input, false)
+}
+
+function solve(input, areCorrectPages) {
   const { rules, pages } = parse(input)
   const ruleMap = constructRuleMap(rules)
   return pages
-    .filter(page => !areCorrectPages(page, ruleMap))
-    .map(incorrect => fix(incorrect, ruleMap))
-    .map(correct => correct[Math.floor(correct.length / 2)])
-    .reduce((acc, n) => acc + n, 0)
+    .map(page => [...page].sort((a, b) => ruleMap[a]?.has(b) ? -1 : 1))
+    .filter((sorted, i) => (sorted.toString() === pages[i].toString()) === areCorrectPages)
+    .reduce((acc, sorted) => acc + sorted[Math.floor(sorted.length / 2)], 0)
 }
 
 function constructRuleMap(rules) {
@@ -21,35 +20,6 @@ function constructRuleMap(rules) {
     ruleMap[from] = ruleMap[from] ? ruleMap[from].add(to) : new Set([to])
     return ruleMap
   }, {})
-}
-
-function areCorrectPages(pages, ruleMap) {
-  const visited = new Set()
-  for (const page of pages) {
-    if (ruleMap[page]?.intersection(visited).size) return false
-    visited.add(page)
-  }
-  return true
-}
-
-function fix(incorrect, ruleMap) {
-  const visited = new Set()
-  let fixed = []
-  for (const page of incorrect) {
-    const conflict = ruleMap[page]?.intersection(visited).values().next().value
-    if (conflict) {
-      const i = fixed.indexOf(conflict)
-      fixed.push(fixed[i])
-      fixed[i] = page
-      if (!areCorrectPages(fixed, ruleMap)) {
-        fixed = fix(fixed, ruleMap)
-      }
-    } else {
-      fixed.push(page)
-    }
-    visited.add(page)
-  }
-  return fixed
 }
 
 function parse(input) {
