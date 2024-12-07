3427b76accb32ef30c95d1210264d031f65111a7
Péter Tamás <kondvik@gmail.com>
2024-12-07 16:32:54 +0100

    refactor(2024/js): Refactor dec_07 solution

--- 2024/js/dec_07/dec07.js
+++ 2024/js/dec_07/dec07.js
@@ -1,28 +1,22 @@
 export function part1(input) {
-  return solve(input, false)
+  return solve(input, [(a, b) => a + b, (a, b) => a * b])
 }
 
 export function part2(input) {
-  return solve(input, true)
+  return solve(input, [(a, b) => a + b, (a, b) => a * b, (a, b) => Number(a.toString() + b.toString())])
 }
 
-function solve(input, withConcatenation) {
+function solve(input, operations) {
   return parse(input)
-    .filter(([res, ...nums]) => isValid(res, nums, withConcatenation))
-    .reduce((acc, calibration) => acc + calibration[0], 0)
+    .filter(([res, ...nums]) => isValid(res, nums, operations))
+    .reduce((acc, [res, _]) => acc + res, 0)
 }
 
-function isValid(res, nums, withConcatenation = false, i = 1, currentRes = nums[0]) {
-  const sum = currentRes + nums[i]
-  const mul = currentRes * nums[i]
-  const concat = withConcatenation && Number(currentRes.toString() + nums[i].toString())
-  if (i === nums.length - 1) return sum === res || mul === res || concat === res
-  const sumPath = isValid(res, nums, withConcatenation, i + 1, sum)
-  const mulPath = isValid(res, nums, withConcatenation, i + 1, mul)
-  const concatPath = withConcatenation && isValid(res, nums, withConcatenation, i + 1, concat)
-  return sumPath || mulPath || concatPath
+function isValid(res, nums, operations, i = 1, currentRes = nums[0]) {
+  const results = operations.map(op => op(currentRes, nums[i]))
+  if (i === nums.length - 1) return results.some(r => r === res)
+  return results.some(r => isValid(res, nums, operations, i + 1, r))
 }
-
 
 function parse(input) {
   return input
