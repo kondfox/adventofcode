a0328551615303b554ea9588ef6c0e182f8d12a3
Péter Tamás <kondvik@gmail.com>
2024-12-09 10:21:08 +0100

    feat(2024/js): Add fequently useful helper functions to tools.js

---
+++ 2024/js/tools.js
@@ -0,0 +1,11 @@
+export const isValidCoord = stringArray => n => {
+  return n[0] >= 0 && n[0] < stringArray.length && n[1] >= 0 && n[1] < stringArray[0].length
+}
+
+export const windowed = (array, size) => {
+  return array.map((_, i) => array.slice(i, i + size)).filter(a => a.length === size)
+}
+
+export const allPairs = array => {
+  return array.flatMap((x, i) => array.slice(i + 1).map(y => [x, y]))
+}
