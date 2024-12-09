0de08a7f04439e735883095ba43048dcc251423e
Péter Tamás <kondvik@gmail.com>
2024-12-09 10:21:38 +0100

    refactor(2024/js): Refactor dec_08 solution

--- 2024/js/dec_08/dec08.js
+++ 2024/js/dec_08/dec08.js
@@ -1,3 +1,5 @@
+import { allPairs, isValidCoord } from '../tools.js'
+
 export function part1(input) {
   return solve(input, 1, 1)
 }
@@ -7,41 +9,34 @@ export function part2(input) {
 }
 
 function solve(input, distanceFrom, distanceTill) {
-  const isValid = n => n[0] >= 0 && n[0] < input.length && n[1] >= 0 && n[1] < input[0].length
   return Object.values(parse(input))
-    .map(nodes => findAntinodes(nodes, distanceFrom, distanceTill, isValid))
-    .reduce((acc, antinodes) => acc.union(antinodes), new Set())
+    .map(nodes => findAntinodes(nodes, distanceFrom, distanceTill, isValidCoord(input)))
+    .reduce((acc, antinodes) => acc.union(antinodes))
     .size
 }
 
 function findAntinodes(nodes, from, till, isValid) {
-  const antinodes = new Set()
-  for (let i = 0; i < nodes.length; i++) {
-    for (let j = i + 1; j < nodes.length; j++) {
-      antiNodePositions(nodes[i], nodes[j], from, till, isValid)
-        .forEach(pos => antinodes.add(pos.toString()))
-    }
-  }
-  return antinodes
+  return allPairs(nodes, 2).reduce((antinodes, [n1, n2]) => {
+    return antinodes.union(antiNodePositions(n1, n2, from, till, isValid))
+  }, new Set())
 }
 
 function antiNodePositions(n1, n2, from, till, isValid) {
-  const yDiff = n2[0] - n1[0]
-  const xDiff = n2[1] - n1[1]
-  return [
-    ...findNodes(n1, xDiff, yDiff, -1, from, till, isValid),
-    ...findNodes(n2, xDiff, yDiff, 1, from, till, isValid),
-  ]
+  const diff = [n2[0] - n1[0], n2[1] - n1[1]]
+  return new Set([
+    ...findNodes(n1, diff, -1, from, till, isValid),
+    ...findNodes(n2, diff, 1, from, till, isValid),
+  ].map(n => n.toString()))
 }
 
-function findNodes(source, xDiff, yDiff, dir, from, till, isValid, nodes = []) {
-  const node = findNode(source, xDiff, yDiff, dir, from)
+function findNodes(source, diff, dir, from, till, isValid, nodes = []) {
+  const node = findNode(source, diff, dir, from)
   if (from > till || !isValid(node)) return nodes
-  return findNodes(source, xDiff, yDiff, dir, from + 1, till, isValid, [...nodes, node])
+  return findNodes(source, diff, dir, from + 1, till, isValid, [...nodes, node])
 }
 
-function findNode(source, xDiff, yDiff, dir, steps) {
-  return [source[0] + yDiff * dir * steps, source[1] + xDiff * dir * steps]
+function findNode(source, diff, dir, steps) {
+  return [source[0] + diff[0] * dir * steps, source[1] + diff[1] * dir * steps]
 }
 
 function parse(input) {
