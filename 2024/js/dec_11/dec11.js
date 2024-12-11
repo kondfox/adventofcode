b4341e2a2ee0a5b01a50f2439d4f7223c547c23f
Péter Tamás <kondvik@gmail.com>
2024-12-11 21:13:42 +0100

    feat(2024/js): Add dec_11 solution

--- 2024/js/dec_11/dec11.js
+++ 2024/js/dec_11/dec11.js
@@ -1,23 +1,58 @@
 export function part1(input) {
+  return solve(input, 25)
+}
+
+export function part2(input) {
+  return solve(input, 75)
+}
+
+function solve(input, limit) {
+  const memo = parse(input).reduce((mem, n) => {
+    mem[n] = node(n, 0)
+    return mem
+  }, {})
+
+  for (let i = 1; i <= limit; i++) {
+    Object.values(memo)
+      .filter(({ visited }) => visited[i - 1])
+      .forEach(({ next, visited }) => {
+        next.forEach(n => visit(n, i, visited[i - 1], memo))
+      })
+  }
+
+  return Object.values(memo)
+    .filter(node => node.visited[limit])
+    .reduce((acc, node) => acc + node.visited[limit], 0)
+}
+
+function next(n) {
   const rules = [
     n => n === '0',
     n => n.length % 2 === 0,
     n => true,
   ]
   const actions = [
-    n => '1',
+    n => ['1'],
     n => [n.substring(0, n.length / 2), n.substring(n.length / 2)].map(Number).map(n => n.toString()),
-    n => (Number(n) * 2024).toString(),
+    n => [(Number(n) * 2024).toString()],
   ]
-  let numbers = parse(input)
-  for (let i = 0; i < 25; i++) {
-    numbers = numbers.flatMap(n => actions[rules.findIndex(rule => rule(n))](n))
-  }
-  return numbers.length
+  return actions[rules.findIndex(rule => rule(n))](n)
 }
 
-export function part2(input) {
+function node(n, i, inc = 1) {
+  return { n, next: next(n), visited: { [i]: inc } }
+}
 
+function visit(n, i, inc, memo) {
+  if (!memo[n]) {
+    memo[n] = node(n, i, inc)
+  } else {
+    if (memo[n].visited[i]) {
+      memo[n].visited[i] += inc
+    } else {
+      memo[n].visited[i] = inc
+    }
+  }
 }
 
 function parse(input) {
